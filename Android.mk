e842cc14eb6b2506ccfa554b694f51c00d7dff7a
Mihai Serban <mihai.serban@intel.com>
2015-03-08 23:51:46 +0200

    Provide ART optimization for host/target release/debug 32/64bit runtimes
    Tracked-On: https://jira01.devtools.intel.com/browse/A01-4
    Change-Id: I95e7b2eb626047541d7b9ad53583864714c5cc95
    Signed-off-by: Mihai Serban <mihai.serban@intel.com>

--- Android.mk
+++ Android.mk
@@ -2,10 +2,49 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libart-extension
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional                                                                                                                                                                                   
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := intel
-LOCAL_SRC_FILES := libart-extension.so
+LOCAL_SRC_FILES_32 := target/x86/libart-extension.so
+LOCAL_SRC_FILES_64 := target/x86_64/libart-extension.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := plugins
+LOCAL_MULTILIB := both
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libart-extension
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := intel
+LOCAL_SRC_FILES_32 := host/x86/libart-extension.so
+LOCAL_SRC_FILES_64 := host/x86_64/libart-extension.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_RELATIVE_PATH := plugins
+LOCAL_MULTILIB := both
+LOCAL_IS_HOST_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libartd-extension
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := intel
+LOCAL_SRC_FILES_32 := target/x86/libartd-extension.so
+LOCAL_SRC_FILES_64 := target/x86_64/libartd-extension.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_RELATIVE_PATH := plugins
+LOCAL_MULTILIB := both
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libartd-extension
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := intel
+LOCAL_SRC_FILES_32 := host/x86/libartd-extension.so
+LOCAL_SRC_FILES_64 := host/x86_64/libartd-extension.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_RELATIVE_PATH := plugins
+LOCAL_MULTILIB := both
+LOCAL_IS_HOST_MODULE := true
+include $(BUILD_PREBUILT)
